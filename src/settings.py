dda9d1111ee6c70548b2ca7537b14a866a737cf4
roipaname <roipaname@gmail.com>
2026-04-06 11:40:15 +0200

    creating settings

---
+++ src/settings.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+import os
+
+
+
+# ──────────────────────────────────────────────
+# Data settings
+# ──────────────────────────────────────────────
+BASE_DIR=Path(__file__).resolve().parent.parent
+DATA_DIR=BASE_DIR / "data"
+RAW_DATA_DIR=DATA_DIR / "raw"
+GENRES_DATA_DIR=RAW_DATA_DIR / "topics"
+PROCESSED_DATA_DIR=DATA_DIR / "processed"
