7d454e05b49cf1138ac471b34818b9a2e2c364b4
roipaname <roipaname@gmail.com>
2026-04-06 11:41:23 +0200

    optimising settings

--- src/settings.py
+++ src/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 
-
+from loguru import logger
 
 # ──────────────────────────────────────────────
 # Data settings
@@ -11,3 +11,6 @@ DATA_DIR=BASE_DIR / "data"
 RAW_DATA_DIR=DATA_DIR / "raw"
 GENRES_DATA_DIR=RAW_DATA_DIR / "topics"
 PROCESSED_DATA_DIR=DATA_DIR / "processed"
+CHECKPOINTS_DIR=BASE_DIR / "checkpoints"
+LOGS_DIR=BASE_DIR / "logs"
+REPORTS_DIR=BASE_DIR / "reports"
